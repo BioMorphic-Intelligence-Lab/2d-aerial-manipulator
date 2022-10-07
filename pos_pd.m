3297324f12290adf0c782d23a5a0423bffa7a92a
Anton <a.bredenbeck@tudelft.nl>
2022-10-07 09:42:56 +0200

    Angle thresholding in reference angle controller

--- pos_pd.m
+++ pos_pd.m
@@ -4,5 +4,9 @@ function [angle] = pos_pd(x, m, des_pos)
 
 angle = (0.2 * (x(8) - des_pos) + 0.25 * x(15));
 
+% Max Min Thresholding
+angle = min(angle, 60*pi/180);
+angle = max(angle, -60*pi/180);
+
 end
 
