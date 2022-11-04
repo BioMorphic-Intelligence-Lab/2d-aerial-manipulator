89b32ea6a6805bc542fe564eef062e81bf5f20d5
Anton <a.bredenbeck@tudelft.nl>
2022-11-04 16:22:04 +0100

    Unify controllers in one matlab class

--- matlab_impl/ctrl.m
+++ matlab_impl/ctrl.m
@@ -1,26 +1,106 @@
-function u = ctrl(x,p_des,m_base,m_link, r,l)
-%CTRL Chains the various controllers to compute the control action.
-% x = Current System State (integral, value, derivative)
-% p_des = Current Desired Base Pose
-% m_base = Mass of the base
-% m_link = Mass of one individual link
-% r = Rotor arm length
-% l = Individual Link Length
+classdef ctrl < handle
+    %F_CTRL Ctrl instance to regulate contact force
+    properties
+        u_last 
+        q_dot_last
+        t_last
+        e_f_last
+    end
+    
+    methods
+        function obj = ctrl()
+            %F_CTRL Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.u_last = zeros(4,1);
+            obj.q_dot_last = zeros(7,1);
+            obj.t_last = 0;
+            obj.e_f_last = zeros(2,1);
+        end
+        
+        function u = pos_ctrl(obj,x,p_des,m_base,m_link, r,l)
+        %CTRL Chains the various controllers to compute the control action.
+        % x = Current System State (integral, value, derivative)
+        % p_des = Current Desired Base Pose
+        % m_base = Mass of the base
+        % m_link = Mass of one individual link
+        % r = Rotor arm length
+        % l = Individual Link Length        
+            % Desired base position
+            p_base = ee2base(p_des,x(11:14),l);
+            
+            % Control Action
+            u = [max(min(angle_lqri(x, m_base, m_link, r, l,...
+                                      pos_pd(x, p_base(1)) ...
+                                         )...
+                            + height_lqr(x, m_base + 4*m_link, p_base(2)),...
+                            [5;5]),...
+                           0);    % Bi-Rotor Inputs
+                           [0;3]... Tendon Inputs
+                        ];
+        
+        
+        end
 
 
-% Desired base position
-p_base = ee2base(p_des,x(11:14),l);
 
-% Control Action
-u = [max(min(angle_lqri(x, m_base, m_link, r, l,...
-                          pos_pd(x, p_base(1)) ...
-                             )...
-                + height_lqr(x, m_base + 4*m_link, p_base(2)),...
-                [5;5]),...
-               0);    % Bi-Rotor Inputs
-               [0;3]... Tendon Inputs
-            ];
+        function u = f_ctrl(obj,t, x,dir,f_des,m_base,m_link,r,l,r_tendon)
+        %F_CTRL Function that follows a direction until it establishes contact and
+        %then tries to regulate the contact force
+        % x = Current System State (integral, value, derivative)
+        % dir = Desired EE position before contact is established
+        % f_des = Desired Contact Force
+        % p_des = Current Desired Base Pose
+        % m_base = Mass of the base
+        % m_link = Mass of one individual link
+        % r = Rotor arm length
+        % l = Individual Link Length
+            
+            % Extract state variables 
+            q = x(8:14);
+            q_dot = x(15:21);
+            
+            % Numerically differentiate velocity to find acceleration
+            q_ddot = (q_dot - obj.q_dot_last)/(t - obj.t_last);
 
+            % Estimate contact force
+            f_contact = estimate_force(q,q_dot,q_ddot,obj.u_last,m_base,m_link,r,l,r_tendon);
+            
+            % Find EE-Pos
+            ee = q(1:2);
+            R = [cos(q(3)), -sin(q(3));
+                 sin(q(3)), cos(q(3))];
+            
+            for k=1:4
+                R = R*[cos(q(3+k)), -sin(q(3+k));
+                       sin(q(3+k)), cos(q(3+k))];
+                ee = ee + R*[0;-l];
+            end
 
+            % We only do force regulation if we are in contact
+            if ee(1) >= 3
+                % Find the force error
+                e_f = f_contact - f_des;
+                % Rate of change of force error
+                e_f_dot = (e_f - obj.e_f_last)/(t-obj.t_last);
+                % Find the relative position change PD to force error
+                delta_p = 0.2 * e_f + 0.1 * e_f_dot;
+                % Control Action
+                u = ctrl(x,dir,m_base,m_link,r,l);
+                % Remember e_f for next time
+                obj.e_f_last = e_f;
+                
+                
+            % Otherwise we simply do the position control
+            else
+                u = ctrl(x,dir,m_base,m_link,r,l);
+            end
+
+            % Remember values for next iteration
+            obj.t_last = t;
+            obj.q_dot_last = q_dot;
+            obj.u_last = u;
+
+end
+    end
 end
 
--- matlab_impl/main.m
+++ matlab_impl/main.m
@@ -13,9 +13,9 @@ r_tendon = 0.05; %m
 
 % Description of the contact wall
 walls = [-1, 0; % Normal Vector
-          3, 0;]; % Support Point
-          %0, 1; % Normal Vector
-          %0, 0]; % Support Point
+          3, 0; % Support Point
+          0, 1; % Normal Vector
+          0, 0]; % Support Point
 % Make sure normal vector is normalized
 walls(1:2:length(walls),:) = walls(1:2:length(walls),:)/...
                              norm(walls(1:2:length(walls),:));
@@ -27,10 +27,10 @@ p_des = @(t) (t <=10) .* [3; 2] + ...
 f_des  = [-1; 0];
 
 % Instantiate ctrl instance
-c = f_ctrl();
+c = ctrl();
 
 % Control Law
-u = @(x,t) [0;0;0;2.5] ;%c.ctrl(t,x,p_des(t),f_des,m_base,m_link,r,l,r_tendon); %ctrl(x, p_des(t), m_base,m_link,r,l);
+u = @(x,t) c.pos_ctrl(x, p_des(t), m_base,m_link,r,l); %c.f_ctrl(t,x,p_des(t),f_des,m_base,m_link,r,l,r_tendon);
 
 % Dynamic model function
 f = @(t, x) am(x, u(x,t), m_base, m_link, r, l,r_tendon, p_des(t), walls);
@@ -40,7 +40,7 @@ f = @(t, x) am(x, u(x,t), m_base, m_link, r, l,r_tendon, p_des(t), walls);
 y0 = [0, 0, 0, 0, 0, 0, 0  ... Integral 
       0, 1, 0, 0, 0, 0, 0 ... Position
       0, 0, 0, 0, 0, 0, 0]; ... Velocity
-tspan = 0:0.01:20;
+tspan = 0:0.01:5;
 
 % Simulate system
 [t, y] = ode45(f, tspan, y0);
